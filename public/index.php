b64df9002134f4b819bcabb6b1811e56ff930a62
miranto117 <tojo.mirantoratsimbazafy@gmail.com>
2024-02-06 18:10:31 +0100

    first twig

--- public/index.php
+++ public/index.php
@@ -7,3 +7,9 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+// if ($_SERVER['APP_DEBUG']) {
+//     umask(0000);
+
+//     Debug::enable();
+// }
